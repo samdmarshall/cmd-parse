d966f67f60819f6ad66be93cfa08f76ef5bc5847
Samantha Marshall <me@samdmarshall.com>
2017-04-23 11:16:55 -0400

    Adding new measurement

--- cmdparse.nim
+++ cmdparse.nim
@@ -22,12 +22,14 @@ try:
   let args = parseCmdLine(input)
   if database_name.len > 0:
     let command = args[args.low()]
-    let arguments = args[1..args.high()].join(" ")
+    let args_array = args[1..args.high()]
+    let arguments = args_array.join(" ")
     
     let influxdb = InfluxDB(protocol: HTTP, host: "localhost", port: 8086)
     
     let values = @{
       "command": command,
+      "arg_count": $len(args_array),
       "arguments": arguments,
       "location": getEnv("PWD"),
     }.toTable
